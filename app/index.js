86b41ae73f5b06c3314d962d826f83b4400c3afb
bharat2005 <bharatvdeshm2005@gmail.com>
2025-07-13 23:22:16 +0530

    project setup done

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { ActivityIndicator, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <ActivityIndicator color='pink' size={'large'} />
+    </View>
+  );
+}
