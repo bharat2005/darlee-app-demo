535673bf66dc8873c8404fdaccf9b759106fab8c
bharat2005 <bharatvdeshm2005@gmail.com>
2025-07-21 17:37:00 +0530

    feat: enhance End component in ProfileBuild with improved Lottie animation, additional motivational text, and a styled button for submission

--- src/components/ProfileBuild/End.js
+++ src/components/ProfileBuild/End.js
@@ -15,9 +15,23 @@ const End = ({width, handleSubmit, isSubmitting}) => {
 
         <Text style={{fontFamily:'Outfit-Medium', fontSize:24, color:'black', textAlign:'center'}}>Sweet, you are all set!</Text>
 
-        <View style={{alignItems:'center', marginTop:24}}>
-        <LottieView source={require('../../../assets/lottie/flowerAnimation.json')} autoPlay loop />
-        </View>
+      
+        <LottieView style={{alignSelf:'center', marginVertical:22, height:120, width:120}} source={require('../../../assets/lottie/flowerAnimation.json')} autoPlay loop />
+    
+        <Text style={{fontFamily:'Outfit-Light', fontSize:16, color:'gray', textAlign:'center'}}>From little cramps to cloudy days, every part matters</Text>
+
+
+        <Button mode='contained'
+        labelStyle={{fontFamily:'Outfit-Medium', fontSize:16, color:'white'}}
+        style={{backgroundColor:isSubmitting ? 'lightgrey' : MyColors.DARK_GREY, height:44, marginTop:24}}
+        contentStyle={{height:44}}
+        theme={{roundness:2}} 
+         onPress={handleSubmit} 
+         disabled={isSubmitting} 
+         loading={isSubmitting} 
+         >
+         Let’s Begin
+        </Button>
 
       {/* <Button mode='contained' onPress={handleSubmit} disabled={isSubmitting} loading={isSubmitting} >
         Care me!
