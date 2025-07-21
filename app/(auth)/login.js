eff5f7dbb76388900121a79178ee3357a58dca55
bharat2005 <bharatvdeshm2005@gmail.com>
2025-07-21 09:19:22 +0530

    feat: enhance login screen with background image and add TransHeader component; update GoogleAuthComp for improved styling and functionality

--- app/(auth)/login.js
+++ app/(auth)/login.js
@@ -1,13 +1,17 @@
-import { View, Text, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { View, Text, TouchableWithoutFeedback, Keyboard, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import GoogleAuthComp from '../../src/components/Auth/GoogleAuthComp'
 import EmailAuthComp from '../../src/components/Auth/EmailAuthComp'
+import TransHeader from '../../src/components/Shared/TransHeader'
 
 const Login = () => {
   return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
     <SafeAreaView style={{flex:1, gap:28}}>
+      <Image source={require('../../assets/images/onboardBack.jpg')} style={{width:100, height:100}} />
+      <TransHeader />
+
     <EmailAuthComp origin={'login'} />
 
     <GoogleAuthComp />
--- src/components/Auth/GoogleAuthComp.js
+++ src/components/Auth/GoogleAuthComp.js
@@ -1,20 +1,22 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useAuth } from '../../contexts/AuthContextProvider'
-import GoogleButton from 'react-native-google-button'
+import MyColors  from '../../constants/MyColors'
+import Google from '../../../assets/svgs/Google'
+
 
 const GoogleAuthComp = () => {
-    //const {googleLogin} = useAuth()
-  return (
-    <View style={{width:'100%', paddingHorizontal:40}}>
+    const {googleLogin} = useAuth()
 
-        <GoogleButton
-        onPress={()=> console.log('google')}
-    />
-{/*         
-        <TouchableOpacity onPress={googleLogin} style={{width:'100%', height:'100%', borderRadius:12, backgroundColor:'snow', borderWidth:1, borderColor:'black', justifyContent:'center', alignItems:'center'}}>
-            <Text style={{color:'black'}}>Google</Text>
-        </TouchableOpacity> */}
+  return (
+    <View style={{width:'100%', paddingHorizontal:70, height:50}}>
+        
+        <TouchableOpacity  activeOpacity={0.8} onPress={googleLogin} style={{width:'100%', height:'100%', borderRadius:32, backgroundColor:'white', justifyContent:'center', alignItems:'center', flexDirection:'row', gap:12, elevation:4}}>
+           <View style={{backgroundColor:'white', padding:8, borderRadius:'50%', position:'absolute', left:8}}>
+           <Google height={24} width={24} fill={MyColors.DARK_GREY} />
+           </View>
+            <Text style={{color:'black', fontSize:17, fontFamily:'Outfit-Light'}}>Continue with Google</Text>
+        </TouchableOpacity>
 
     </View>
   )
